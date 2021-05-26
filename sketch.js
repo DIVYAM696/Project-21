676baef38b63e9d2c95498c5d6a569f8130fd737
DIVYAM696 <83130540+DIVYAM696@users.noreply.github.com>
2021-05-26 20:19:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,87 @@
+var canvas;
+var block1,block2,block3,block4;
+var ball, edges;
+var music;
+
+function preload(){
+    // load sound here
+music = loadSound("music.mp3")
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+    block1 = createSprite(10,580,360,30);
+    block1.shapeColor = "blue";
+
+    block2 = createSprite(295,580,200,30);
+    block2.shapeColor = "orange";
+
+    block3 = createSprite(500,580,200,30);
+    block3.shapeColor = "red";
+
+    block4 = createSprite(705,580,200,30);
+    block4.shapeColor = "green";
+
+    //music.addSound = ("music.mp3",music);
+    //music.stop
+    //create two more blocks i.e. block3 and block4 here
+
+    ball = createSprite(random(20,750),100, 40,40);
+    ball.shapeColor = "purple"
+   
+    ball.velocityX = 5;
+    ball.velocityY = 7;
+    //write code to add velocityX and velocityY
+
+}
+
+function draw() {
+    background(rgb(169,169,169));
+    edges=createEdgeSprites();
+    ball.bounceOff(edges);
+    
+
+   
+
+    
+    //write code to bounce off ball from the block1 
+    
+        //write code to set velocityX and velocityY of ball as 0
+
+        //write code to stop music
+    
+
+    //write code to bounce off ball from the block3
+    
+    if(ball.isTouching(block1) && ball.bounceOff(block1)){
+        ball.shapeColor = "yellow"; 
+        music.play(); 
+       }
+
+       if(ball.isTouching(block2)){
+           ball.shapeColor = "green"; 
+           ball.velocityX = 0;
+           ball.velocityY = 0;
+           music.stop(); 
+          }
+
+          if(ball.isTouching(block3) && ball.bounceOff(block3)){
+           ball.shapeColor = "red"; 
+           music.play(); 
+          }
+
+          if(ball.isTouching(block4) && ball.bounceOff(block4)){
+           ball.shapeColor = "blue"; 
+           music.play(); 
+          }
+        
+
+    
+
+    //write code to bounce off ball from the block4
+
+    drawSprites();
+    
+}
